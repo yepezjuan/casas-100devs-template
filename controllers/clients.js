a0955167154f33131d9dbb2cb10e53d57a1c1a21
juanyep <yepezjuan6@gmail.com>
2026-06-03 21:55:28 -0700

    added sort clients by service day with color differentation

--- controllers/clients.js
+++ controllers/clients.js
@@ -4,13 +4,45 @@ module.exports = {
     console.log(req.user);
     try {
       const clients = await Client.find({ userId: req.user.id });
+      const mondayClients = await Client.find({
+        userId: req.user.id,
+        day: "Monday",
+      });
+
+      const tuesdayClients = await Client.find({
+        userId: req.user.id,
+        day: "Tuesday",
+      });
+      const wednesdayClients = await Client.find({
+        userId: req.user.id,
+        day: "Wednesday",
+      });
+      const thursdayClients = await Client.find({
+        userId: req.user.id,
+        day: "Thursday",
+      });
+      const fridayClients = await Client.find({
+        userId: req.user.id,
+        day: "Friday",
+      });
+      const saturdayClients = await Client.find({
+        userId: req.user.id,
+        day: "Saturday",
+      });
       const clientsLeft = await Client.countDocuments({
         userId: req.user.id,
         completed: false,
       });
+
       res.render("clients.ejs", {
         clients: clients,
         left: clientsLeft,
+        mondayClients: mondayClients,
+        tuesdayClients: tuesdayClients,
+        wednesdayClients: wednesdayClients,
+        thursdayClients: thursdayClients,
+        fridayClients: fridayClients,
+        saturdayClients: saturdayClients,
         user: req.user,
       });
     } catch (err) {
